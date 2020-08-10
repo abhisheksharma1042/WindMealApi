f75d382f62bfbc3d74eb6b1d05d811b957d1890b
abhisheksharma1042 <abhisheksharma1042@users.noreply.github.com>
2020-08-10 15:11:46 -0700

    Populating user with name on reviews get call

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,7 @@ exports.register = asyncHandler(async (req, res, next) => {
 //@route  POST /api/v1/auth/login
 //@access Public
 exports.login = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
   const { email, password } = req.body;
 
   // Validate email and password
